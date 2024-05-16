7e82c4812c9ecb9838917a76223b505617f2d8da
joshua boyi <boyijoshua72@gmail.com>
2024-05-16 11:11:54 +0100

    new

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const db = require("../main/Database/index.js");
+const User = db.User;
+const port = process.env.PORT || 4000;
+const ApiRoute = require("../main/MainRouter.js");
+// require('./API/Controllers/AssignmentController.js')
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", ApiRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Qitt Api V1.0");
+});
+
+// Server Running
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
